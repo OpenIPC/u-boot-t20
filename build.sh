742147b005eff0e8e2729b45dacf8bedb9f43386
Dmitry Ermakov <de@dimer.org.ua>
2022-07-07 21:27:43 +0300

    Add build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+export ARCH=mips
+export CROSS_COMPILE=${CROSS_COMPILE:-mips-linux-gnu-}
+
+OUTPUTDIR="${HOME}/src/hisi/uboot"
+SOCS="t10 t20"
+
+for soc in ${SOCS};do
+
+make distclean
+make isvp_${SOC}_sfcnor
+make -j 8
+
+cp u-boot-with-spl.bin u-boot-${soc}.bin
+cp u-boot-${soc}.bin ${OUTPUTDIR}/u-boot-${soc}-universal.bin
+
+done
